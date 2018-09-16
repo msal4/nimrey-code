144682b91ceeb9cbfc1d3b3d77d754ef6c1fb81c
Mohammed Salman <msal4@outlook.com>
2018-09-16 18:13:09 +0300

    commented containterRule to prevent the social icons in the header from disappearing on mobile screens

--- src/components/header.js
+++ src/components/header.js
@@ -6,15 +6,15 @@ import { rhythm } from '../utils/typography';
 import SocialLinks from '../components/social-links';
 
 const linkRule = css({ float: 'right', marginLeft: ".5rem", color: '#a29bfe', textDecoration: 'none', });
-const contaierRule = css({
-  '& .social-links': {
-    '@media screen and (max-width: 800px)': {
-      display: 'none',
-    },
-  },
-});
+// const contaierRule = css({
+//   '& .social-links': {
+//     '@media screen and (max-width: 800px)': {
+//       display: 'none',
+//     },
+//   },
+// });
 const Header = ({ data }) => (
-  <div {...contaierRule}>
+  <div /*{ ...contaierRule} */ >
     <Link to={`/`}>
       <g.H3
         marginBottom={rhythm(2)}
